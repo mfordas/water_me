abdc4495a7f9eeb8cb6c217afb99b79508d87b1a
mfordas <mateusz.fordas@gmail.com>
2021-04-10 12:33:25 +0200

    Change restart rules

--- backend/middleware/authorization.ts
+++ backend/middleware/authorization.ts
@@ -9,9 +9,6 @@ const auth = (req: Request, res: Response, next: NextFunction): void => {
   const requestWithType = req as IBodyRequestForAuthComp;
   const token = req.header('x-auth-token');
 
-  console.log(token);
-  console.log(process.env.JWTPRIVATEKEY);
-
   if (!token) res.status(401).send('Access denied. No token provided.');
 
   if (token && process.env.JWTPRIVATEKEY) {
